74af8bfa3754f5bfa7554c50b3f159baeb869cbf
FlexxTech <contato.flexxtech@gmail.com>
2025-01-19 22:19:02 -0300

    Adding tests & Updating create endpoint

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_task():
     task_id_control+=1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message":"Nova tarefa criada com sucesso"})
+    return jsonify({"message":"Nova tarefa criada com sucesso","id":new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -40,13 +40,14 @@ def update_task(id):
             task = t
             break
 
-        if task == None:
-            return jsonify({"message":"Tarefa não encontrada"}),404
-        data = request.get_json()
-        task.title = data['title']
-        task.description = data['description']
-        task.completed = data['completed']
-        return jsonify({"message":"Tarefa atualizada com sucesso"})
+    if task == None:
+        return jsonify({"message":"Tarefa não encontrada"}),404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({"message":"Tarefa atualizada com sucesso"})
 
 
 @app.route('/tasks/<int:id>',methods=['DELETE'])
